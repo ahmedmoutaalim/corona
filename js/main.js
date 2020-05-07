653a4910de4b296b1bbf7dfe99adf15dc3c5820b
Omar <Omar@omar.com>
2020-05-07 02:17:02 +0000

    last modification

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 //--------------------selectors-----------------:
 
-const testButton = document.getElementById('start-btn')
-const sectionQuestion = document.getElementById('container')
-const informationSection = document.getElementById('information')
-const scroll = document.querySelectorAll('.steppers h1')
-const nextButton = document.getElementById('next-btn')
-const question = document.getElementById('nextQuestion')
-const selectInputs = document.querySelector('.answer-inputs')
-const buttPrevious = document.getElementById('pré-btn')
-const progressBar = document.getElementsByClassName('progress')
-const serialPlus = document.querySelector('.question-number')
-const bar = document.querySelector('.bar')
+const testButton = document.getElementById('start-btn');
+const sectionQuestion = document.getElementById('container');
+const informationSection = document.getElementById('information');
+const scroll = document.querySelectorAll('.steppers h1');
+const nextButton = document.getElementById('next-btn');
+const question = document.getElementById('nextQuestion');
+const selectInputs = document.querySelector('.answer-inputs');
+const buttPrevious = document.getElementById('pré-btn');
+const progressBar = document.getElementsByClassName('progress');
+const serialPlus = document.querySelector('.question-number');
+const bar = document.querySelector('.bar');
+const title =document.querySelector('.container__titre');
+const controleAnimation=document.querySelector('.animated-element');
+const resultMessage= document.querySelectorAll('.container p');
+
 
 
 let questionIndex=0;
+let knowQuetion={};
+
 
 
 
 //---------------------events-------------------:
-testButton.addEventListener('click' , startTest)
-nextButton.addEventListener('click',nextQuetion)
-selectInputs.addEventListener('change',typeOfButton)
+testButton.addEventListener('click' , startTest);
+nextButton.addEventListener('click',nextQuetion);
+selectInputs.addEventListener('change',typeOfButton);
 
 
 
@@ -30,15 +36,17 @@ selectInputs.addEventListener('change',typeOfButton)
 //-----------------------start_test--------------:
 function startTest(){
 
-    testButton.classList.add('hide')
-    sectionQuestion.classList.remove('hide')
-    informationSection.classList.add('hide')
-    scroll[0].classList.remove('actuel-part')
-    scroll[1].classList.add('actuel-part')
+    testButton.classList.add('hide');
+    sectionQuestion.classList.remove('hide');
+    informationSection.classList.add('hide');
+    scroll[0].classList.remove('actuel-part');
+    scroll[1].classList.add('actuel-part');
                                
    nextButton.disabled=true;
- 
-   controlButtons()
+
+   controlButtons();
+   
+   
 }
 
 //---------------------------next_Question-----------------------:
@@ -47,19 +55,32 @@ function startTest(){
 
 
  function nextQuetion(){
-
-    questionIndex++
-    showQuestion( questions[questionIndex])
-    progressButtons(questionIndex)
+     
+if(questionIndex < 21){
+    questionIndex++;
+    showQuestion( questions[questionIndex]);
+    progressButtons(questionIndex);
 
     nextButton.disabled=true;
+    controlButtons() ;
+    animation('next');
+   
+    if (questionIndex === 21){
+       
+        nextButton.innerHTML='terminer le test';
+        nextButton.classList.add('result');
+        const doneTest = document.querySelector('.result');
+        doneTest.addEventListener('click',comptQuetion);
+       
+    
+    }else{
 
-    controlButtons()
-
-    endTest(questionIndex)
-
-
-
+        nextButton.innerHTML='suivant';
+    }
+}
+  
+   
+  
  }
 
 
@@ -70,7 +91,7 @@ function startTest(){
 
 function showQuestion(questions){
 
-    question.innerText=questions.question
+    question.innerText=questions.question;
     selectInputs.innerHTML = ''
 
 
@@ -80,7 +101,7 @@ function showQuestion(questions){
 
         selectInputs.innerHTML += 
          `<div>
-        <input type="radio"  name="choix" id="${answer.text}">
+        <input type="radio"  name="${questions.input.questionNumber}" id="${answer.text}">
         <label for="${answer.text}">
             <i class="fa ${ answer.icon}"></i>
             <span>${answer.text}</span> </label>
@@ -91,7 +112,7 @@ function showQuestion(questions){
     
     }else{
 
-        selectInputs.innerHTML=` <input type="number"  id="${questions.input.name}"
+        selectInputs.innerHTML=` <input type="number" name="${questions.input.questionNumber}"  id="${questions.input.name}"
          min="${questions.input.min}" max="${questions.input.max}" placeholder=" ${questions.input.min}- ${questions.input.max}">
         <span class="input-span">${questions.input.name}</span>`
         
@@ -106,19 +127,30 @@ function showQuestion(questions){
 
 buttPrevious.addEventListener('click',()=>{
 
-    questionIndex--
+    questionIndex--;
      
-    showQuestion( questions[questionIndex])
+    showQuestion( questions[questionIndex]);
 
-    progressButtons(questionIndex)
+    progressButtons(questionIndex);
 
-    nextButton.disabled=true 
+    nextButton.disabled=true ;
+    animation('previous');
+    controlButtons();
+  
+    if (questionIndex === 21) {
+        nextButton.innerText = 'Terminer le test';
 
-    controlButtons()
+
+
+    } else {
+        nextButton.innerText = 'Suivant';
+        nextButton.classList.remove('result');
+    }
+    
 })
 
 
-//---------------------controle_progressbar----------------------:
+//---------------------controle_progress_bar----------------------:
 
 function progressButtons(number){
 
@@ -126,35 +158,40 @@ function progressButtons(number){
     const addNumber = number + 1 ;
     serialPlus.innerHTML= addNumber ;
 
-    bar.style.width =`calc(${addNumber}*calc(100%/22))` 
+    bar.style.width =`calc(${addNumber}*calc(100%/22))` ;
 
    
 }
+ 
 
 
-
-//----------------------------inclickuble & clickable button--------------:
+//----------------------------inclickable & clickable button--------------:
 function typeOfButton(event){
 
-    const input=event.target
- 
+    const input=event.target;
     
 if (input.type === 'number') {
+
 
 
         const number = parseFloat(input.value)
                     
         if (number >= input.min && number <= input.max) {
-
-            nextButton.disabled = false
+            nextButton.disabled = false;
+            knowQuetion[input.name]=input.value;
         } else {
-            nextButton.disabled = true
+            nextButton.disabled = true;
+              
+
 
         }
 
 } else {
 
-        nextButton.disabled = false
+        nextButton.disabled = false;
+
+        knowQuetion[input.name] = input.id;
+        console.log(knowQuetion);
     }
 
    
@@ -167,32 +204,142 @@ function controlButtons(){
 
     if(questionIndex===0){
 
-        buttPrevious.classList.add('visibility')
+        buttPrevious.style.visibility='hidden';
+ 
+    }else{ 
 
-    }else{
-
-        buttPrevious.classList.remove('visibility')
+        buttPrevious.style.visibility='visible';
 
     }
 
 }
 
 
-//------------ajouter Button terminé
 
 
-function endTest(){
 
-    if(questionIndex===21){
 
-        nextButton.innerText = 'Terminer le test'    
 
-}else{
 
-    nextButton.innerText = 'Suivant';
+
+//------------show result of the quiz:
+
+
+function showResult(damage){
+
+    scroll[1].classList.remove('actuel-part');
+    scroll[2].classList.add('actuel-part');
+    sectionQuestion.classList.add('hide');
+    informationSection.classList.remove('hide');
+    testButton.classList.remove('hide');
+    testButton.innerText='Recommoncer le test';
+    title.innerHTML='resultat';
+    testButton.addEventListener('click',()=>{
+
+        window.location.reload();
+    })
+
+    if (damage === 0) {
+        resultMessage[0].innerText =
+            "Votre situation ne relève probablement pas du Covid-19. N’hésitez pas à contacter votre médecin en cas de doute. Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation. Pour toute information concernant le Covid-19, consulter la page Conseils";
+        resultMessage[1].innerText =
+            "Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.";
+        resultMessage[0].style.fontSize = "25px";
+        resultMessage[0].style.fontWeight = "bold";
+        resultMessage[0].style.color = "#026534";
+    } else if (damage === 1) {
+        resultMessage[0].innerText =
+            "Nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouveau l’application pour réévaluer vos symptômes";
+        resultMessage[1].innerText =
+            "Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.";
+        resultMessage[0].style.fontSize = "25px";
+        resultMessage[0].style.fontWeight = "bold";
+        resultMessage[0].style.color = "#026534";
+    } else if (damage === 2) {
+        resultMessage[0].innerText =
+            "Vous pouvez faire une téléconsultation ou médecin généraliste ou visite à domicile. Appelez le 141 si une gêne respiratoire ou des difficultés importantes pour s’alimenter ou boire pendant plus de 24h apparaissent.";
+        resultMessage[1].innerText =
+            "Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.";
+        resultMessage[0].style.fontSize = "25px";
+        resultMessage[0].style.fontWeight = "bold";
+        resultMessage[0].style.color = "#787878";
+    } else {
+        resultMessage[0].innerText = "Appelez le 141";
+        resultMessage[1].innerText =
+            "Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.";
+    
+        resultMessage[0].style.color = "#d63031";
+        resultMessage[0].style.fontSize = "28px";
+        resultMessage[0].style.fontWeight = "bolder";
+    }
 }
 
+
+
+
+
+//-------------animation----------------:
+function animation(transition){
+
+    controleAnimation.style.animation= `${transition} .6s ease`;
+    controleAnimation.addEventListener('animationend', () => {
+        controleAnimation.style.animation = ``;
+    })    
+    }
+
+
+
+
+let damage = 0; 
+
+
+function comptQuetion(){
+
+    if(knowQuetion['Q1']==='oui'){
+
+        damage++
+    }
+
+    if(parseFloat(knowQuetion['Q2'] >39) || parseFloat(knowQuetion['Q2'] <35)){
+
+        damage++
+    }
+
+
+    if(knowQuetion['Q3']==='oui' ){
+
+        damage++
+
+    }
+
+    if(knowQuetion['Q10']==='Fatigué(e)' || knowQuetion['Q10']=== 'Très fatigué' ){
+
+        damage++
+    }
+
+    if(parseFloat(knowQuetion['Q12'] >120) || parseFloat(knowQuetion['Q12'] < 40)){
+
+        damage++
+    }
+
+    if(knowQuetion['Q16']!=='non'){
+
+        damage++
+    }
+ 
+    showResult(damage)
+   
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -214,6 +361,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q1',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -227,6 +375,7 @@ const questions = [{
 
     input: {
         type: 'number',
+        questionNumber: 'Q2',
         name: 'degrés',
         min: 34,
         max: 42
@@ -236,6 +385,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q3',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -249,6 +399,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q4',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -262,6 +413,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q5',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -275,6 +427,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q6',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -288,6 +441,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q7',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -301,6 +455,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q8',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -314,6 +469,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q9',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -327,6 +483,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q10',
         answer: [{
             text: 'Bien',
             icon: ' far fa-laugh'
@@ -346,6 +503,7 @@ const questions = [{
 
     input: {
         type: 'number',
+        questionNumber: 'Q11',
         name: 'ans',
         min: 15,
         max: 110
@@ -357,6 +515,7 @@ const questions = [{
 
     input: {
         type: 'number',
+        questionNumber: 'Q12',
         name: 'kg',
         min: 20,
         max: 250
@@ -368,6 +527,7 @@ const questions = [{
 
     input: {
         type: 'number',
+        questionNumber: 'Q13',
         name: 'cm',
         min: 80,
         max: 250
@@ -379,6 +539,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q14',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -394,6 +555,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q15',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -409,6 +571,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q16',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -424,6 +587,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q17',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -439,6 +603,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q18',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -454,6 +619,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q19',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -468,6 +634,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q20',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -486,6 +653,7 @@ const questions = [{
 
     input: {
         type: 'radio',
+        questionNumber: 'Q21',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
@@ -497,9 +665,10 @@ const questions = [{
 }, 
 {
     question: 'Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).',
-
+    
     input: {
         type: 'radio',
+        questionNumber: 'Q22',
         answer: [{
             text: 'Oui',
             icon: 'fa-check'
